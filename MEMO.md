9801fd85bcca9ccc61ab4da0717190e23f4a811f
Hoshi <hoshimure.47@gmail.com>
2020-06-13 20:05:39 +0900

    initial commit.

---
+++ MEMO.md
@@ -0,0 +1,45 @@
+## Rust の知識
+
+### 継承（Derive）
+
+- コンパイラには、`[#derive]` アトリビュートを用いることで方に対しての特定のトレイトの標準的な実装を提供する機能がある。
+- 構造体などに振る舞いを追加することができる
+- derive可能なトレイトの一覧
+  - Eq: 同じ値での比較がすべての値で真になる場合に付与できるトレイト（浮動小数点型ではNaN同士の比較が真にならないので実装されていない）
+  - PartialEq: オブジェクト同士が透過であるかの比較を行うためのトレイト
+  - Clone: コピーによって `&T` から `T` を作成するトレイト
+  - Hash: `&T` からハッシュ値を計算するためのトレイト
+  - Debug: `{:?}` フォーマッタを利用して値をフォーマットするためのトレイト
+- [Rustの構造体などに追加できる振る舞いを確認する - Qiita](https://qiita.com/apollo_program/items/2495dda519ae160971ed)
+
+### impl
+
+- Implement some functionally for a type.
+- The `impl` keyword is primarily used to define implementations on types. Inherent implementations are standalone, while trait implementations are used to implement traits for types, ot other traits.
+- [impl - Rust](https://doc.rust-lang.org/beta/std/keyword.impl.html)
+
+```rust
+struct Example {
+  number: i32,
+}
+
+impl Example {
+  fn boo() {
+    println!("oo!)
+  }
+}
+```
+
+## Parser の知識
+
+### 字句解析
+
+- 字句解析とは、ユーザーの入力した文字列からトークン列を切り出す操作。
+- 後に続く構文解析の前段の処理で、トークン列にすることで空白の虫や文字列から数値への変換などの文字列処理を終わらせる。
+
+### トークン
+
+- 言語の文法のうち、終端記号と呼ばれるものがトークン
+- トークンとは、文法において直接文字列として表現されている要素
+- データ型で
+
---
+++ src/main.rs
@@ -0,0 +1,223 @@
+use std::io;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Loc(usize, usize);
+
+impl Loc {
+    fn merge(&self, other: &Loc) -> Loc {
+        use std::cmp::{max, min};
+        Loc(min(self.0, other.0), max(self.1, other.1))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Annotation<T> {
+    value: T,
+    loc: Loc,
+}
+
+impl<T> Annotation<T> {
+    fn new(value: T, loc: Loc) -> Self {
+        Self { value, loc }
+    }
+}
+
+fn main() {
+    use std::io::{stdin, BufRead, BufReader};
+
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let stdin = BufReader::new(stdin);
+    let mut lines = stdin.lines();
+
+    loop {
+        prompt("> ").unwrap();
+        // ユーザの入力を取得する.
+        if let Some(Ok(line)) = lines.next() {
+            // 字句解析を行う.
+            let token = lex(&line);
+            println!("{:?}", token);
+        } else {
+            break;
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum TokenKind {
+    // [0-9][0-9]*
+    Number(u64),
+    // Operator
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    LParen,
+    RParen,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum LexErrorKind {
+    InvalidChar(char),
+    Eof,
+}
+
+type Token = Annotation<TokenKind>;
+type LexError = Annotation<LexErrorKind>;
+
+impl Token {
+    fn number(n: u64, loc: Loc) -> Self {
+        Self::new(TokenKind::Number(n), loc)
+    }
+    fn plus(loc: Loc) -> Self {
+        Self::new(TokenKind::Plus, loc)
+    }
+    fn minus(loc: Loc) -> Self {
+        Self::new(TokenKind::Minus, loc)
+    }
+    fn asterisk(loc: Loc) -> Self {
+        Self::new(TokenKind::Asterisk, loc)
+    }
+    fn slash(loc: Loc) -> Self {
+        Self::new(TokenKind::Slash, loc)
+    }
+    fn lparen(loc: Loc) -> Self {
+        Self::new(TokenKind::LParen, loc)
+    }
+    fn rparen(loc: Loc) -> Self {
+        Self::new(TokenKind::RParen, loc)
+    }
+}
+
+impl LexError {
+    fn invalid_char(c: char, loc: Loc) -> Self {
+        LexError::new(LexErrorKind::InvalidChar(c), loc)
+    }
+    fn eof(loc: Loc) -> Self {
+        LexError::new(LexErrorKind::Eof, loc)
+    }
+}
+
+// 字句解析器
+fn lex(input: &str) -> Result<Vec<Token>, LexError> {
+    // 字句解析結果を保存するベクター.
+    let mut tokens = Vec::new();
+    // 入力値.
+    let input = input.as_bytes();
+    // 読み取り位置.
+    let mut pos = 0;
+
+    // サブレキサーを読んだあとに pos を更新する.
+    macro_rules! lex_a_token {
+        ($lexer:expr) => {{
+            let (tok, p) = $lexer?;
+            tokens.push(tok);
+            pos = p;
+        }};
+    }
+
+    while pos < input.len() {
+        match input[pos] {
+            b'0'...b'9' => lex_a_token!(lex_number(input, pos)),
+            b'+' => lex_a_token!(lex_plus(input, pos)),
+            b'-' => lex_a_token!(lex_minus(input, pos)),
+            b'*' => lex_a_token!(lex_asterisk(input, pos)),
+            b'/' => lex_a_token!(lex_slash(input, pos)),
+            b'(' => lex_a_token!(lex_lparen(input, pos)),
+            b')' => lex_a_token!(lex_rparen(input, pos)),
+            b' ' | b'\n' | b'\t' => {
+                let ((), p) = skip_spaces(input, pos)?;
+                pos = p;
+            }
+            b => return Err(LexError::invalid_char(b as char, Loc(pos, pos + 1))),
+        }
+    }
+
+    Ok(tokens)
+}
+
+// 読み取り位置のバイトを1進める.
+fn consume_byte(input: &[u8], pos: usize, b: u8) -> Result<(u8, usize), LexError> {
+    if input.len() <= pos {
+        return Err(LexError::eof(Loc(pos, pos)));
+    }
+
+    if input[pos] != b {
+        return Err(LexError::invalid_char(input[pos] as char, Loc(pos, pos)));
+    }
+
+    Ok((b, pos + 1))
+}
+
+fn lex_number(input: &[u8], mut pos: usize) -> Result<(Token, usize), LexError> {
+    use std::str::from_utf8;
+
+    let start = pos;
+    let end = recognize_many(input, start, |b| b"0123456789".contains(&b));
+    let n = from_utf8(&input[start..end]).unwrap().parse().unwrap();
+
+    return Ok((Token::number(n, Loc(start, end)), end));
+}
+
+fn lex_plus(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'+').map(|(_, end)| (Token::plus(Loc(start, end)), end))
+}
+
+fn lex_minus(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'-').map(|(_, end)| (Token::minus(Loc(start, end)), end))
+}
+
+fn lex_asterisk(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'*').map(|(_, end)| (Token::asterisk(Loc(start, end)), end))
+}
+
+fn lex_slash(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'/').map(|(_, end)| (Token::slash(Loc(start, end)), end))
+}
+
+fn lex_lparen(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b'(').map(|(_, end)| (Token::lparen(Loc(start, end)), end))
+}
+
+fn lex_rparen(input: &[u8], start: usize) -> Result<(Token, usize), LexError> {
+    consume_byte(input, start, b')').map(|(_, end)| (Token::rparen(Loc(start, end)), end))
+}
+
+fn skip_spaces(input: &[u8], mut pos: usize) -> Result<((), usize), LexError> {
+    let pos = recognize_many(input, pos, |b| b" \n\t".contains(&b));
+
+    return Ok(((), pos));
+}
+
+fn recognize_many(input: &[u8], mut pos: usize, mut f: impl FnMut(u8) -> bool) -> usize {
+    while pos < input.len() && f(input[pos]) {
+        pos += 1;
+    }
+
+    pos
+}
+
+#[test]
+fn test_lexer() {
+    assert_eq!(
+        lex("1 + 2 * 3 - -10"),
+        Ok(vec![
+            Token::number(1, Loc(0, 1)),
+            Token::plus(Loc(2, 3)),
+            Token::number(2, Loc(4, 5)),
+            Token::asterisk(Loc(6, 7)),
+            Token::number(3, Loc(8, 9)),
+            Token::minus(Loc(10, 11)),
+            Token::minus(Loc(12, 13)),
+            Token::number(10, Loc(13, 15))
+        ])
+    )
+}
+
+fn prompt(s: &str) -> io::Result<()> {
+    use std::io::{stdout, Write};
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    stdout.write(s.as_bytes())?;
+    stdout.flush()
+}
